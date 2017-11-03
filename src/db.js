5121240f656c92d9f71616e21285703cb0d4cd4f
jeremymuhia <jmuhia80@gmail.com>
2017-11-03 03:13:49 -0400

    build room

--- src/db.js
+++ src/db.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
     username: String,
     key: String,
-    rooms: [Room]
+    rooms: [String]
 });
 
 const Room = new mongoose.Schema({
+    name: String,
+    members: [String]
+});
+
+const Message = new mongoose.Schema({
 
 });
