f94dc3ecc7526b44ce438f732110003453f5c194
jeremymuhia <jmuhia80@gmail.com>
2017-11-03 03:11:08 -0400

    build room

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
     username: String,
     key: String,
-    rooms: Object
+    rooms: [Room]
 });
 
 const Room = new mongoose.Schema({
-    
+
 });
