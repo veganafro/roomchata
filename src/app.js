dd5d3cd62ca43b7dd68e2356796b6c9f97bfa270
jeremymuhia <jmuhia80@gmail.com>
2017-11-02 23:17:21 -0400

    building app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const body_parser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(reqeust, response) {
+
+});
+
+app.post('/', function(request, response) {
+
+});
