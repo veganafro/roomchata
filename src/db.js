e40ef4c1c0f23df76e9c3dd8da1ca221151e6414
jeremymuhia <jmuhia80@gmail.com>
2017-11-03 03:23:51 -0400

    build room

--- src/db.js
+++ src/db.js
@@ -12,11 +12,15 @@ const User = new mongoose.Schema({
 // this schema models a room
 // it's important to note that the String array `members` will contain IDs of users in the room
 // this can be used to somewhat quickly look up users form the `User` collection
+// but for reasons pertaining to good database design, this String array will likely be excluded
 const Room = new mongoose.Schema({
     name: String,
     users: [String]
 });
 
+// this schema models a message
+// it's important to note that the String `room` will be an ID of the room the message was sent in
+// this can be used to somewhat quickly look up messages that belong in the room
 const Message = new mongoose.Schema({
     room: String,
     sender: String,
