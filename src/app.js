1968c996100794f61916c21d9d825bebf32a6a8d
jeremymuhia <jmuhia80@gmail.com>
2017-11-10 15:40:01 -0500

    milestoning

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const body_parser = require('body-parser');
 
 const app = express();
+
 app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(body_parser.urlencoded({extended: false}));
 
 app.get('/', function(reqeust, response) {
+    response.render('home');
+});
 
+app.get('/search', function(request, response) {
+    response.render('search');
 });
 
 app.post('/', function(request, response) {
