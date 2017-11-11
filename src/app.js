c07b2107e29d22871287aa5b05a394db201d602b
jeremymuhia <jmuhia80@gmail.com>
2017-11-10 19:15:05 -0500

    check ports

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const body_parser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
 app.use(body_parser.urlencoded({extended: false}));
 
 app.get('/', function(reqeust, response) {
@@ -28,5 +28,5 @@ app.get('/room', function(request, response) {
 });
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on port', this.address().port, app.settings.env);
+    console.log('Listening on port', this.address().port, 'in context', app.settings.env);
 });
