190d1f02515cb631636cf2998f82087f905a7d3e
jeremymuhia <jmuhia80@gmail.com>
2017-11-10 19:25:08 -0500

    update to jsx

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const body_parser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
 app.use(body_parser.urlencoded({extended: false}));
 
