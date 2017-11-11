a8e4be528775502c1cd092330a60c37419b731ed
jeremymuhia <jmuhia80@gmail.com>
2017-11-10 19:16:45 -0500

    check ports

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const body_parser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'hbs');
-// app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
 app.use(body_parser.urlencoded({extended: false}));
 
 app.get('/', function(reqeust, response) {
