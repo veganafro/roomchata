2de49360fee3837710c5618832c4c884a2305a91
jeremymuhia <jmuhia80@gmail.com>
2017-11-10 20:13:47 -0500

    reacting hard

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,1 +1,12 @@
-'this is a temporary page';
+const React = require('react');
+const PropTypes = require('prop-types');
+
+class Home extends React.Component {
+    render() {
+        return (
+            <html>
+                
+            </html>
+        );
+    }
+}
