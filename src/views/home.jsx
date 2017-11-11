5428b1ea00378a18ec12a15523bb72eaf4b51013
jeremymuhia <jmuhia80@gmail.com>
2017-11-10 20:28:24 -0500

    reacting hard

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,12 +1,25 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-class Home extends React.Component {
+class Base extends React.Component {
     render() {
         return (
             <html>
-                
+                <head>
+                    <title>{
+                        this.props.title}
+                    </title>
+                </head>
+                <body>
+                    {this.props.children}
+                </body>
             </html>
         );
     }
 }
+
+Base.propTypes = {
+    title: PropTypes.string,
+};
+
+module.exports = Base;
