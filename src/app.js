84c396851ad9ee6a9bce77b373828f94d1b82f4a
jeremymuhia <jmuhia80@gmail.com>
2017-11-18 16:51:46 -0500

    set up firebase

--- src/app.js
+++ src/app.js
@@ -10,6 +10,24 @@ if (app.settings.env === 'development') {
     require('dotenv').config();
 }
 
+var firebase_config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+}
+console.log(firebase_config);
+firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+});
+
 app.set('view engine', 'jsx');
 app.engine('jsx', react_views.createEngine());
 
@@ -38,5 +56,4 @@ app.post('/search', function(request, response) {
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port', this.address().port, 'in context', app.settings.env);
-    console.log(process.env);
 });
