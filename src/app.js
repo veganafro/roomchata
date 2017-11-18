ced8c9e87217d2f4dd9901fe5c6f225356dd53c7
jeremymuhia <jmuhia80@gmail.com>
2017-11-18 16:58:17 -0500

    set up firebase

--- src/app.js
+++ src/app.js
@@ -9,15 +9,7 @@ const app = express();
 if (app.settings.env === 'development') {
     require('dotenv').config();
 }
-var firebase_config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-}
-console.log(firebase_config);
+
 firebase.initializeApp({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
