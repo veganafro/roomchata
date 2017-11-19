856fe7fb7c205948db4c96cbfb54a423b51b4e20
jeremymuhia <jmuhia80@gmail.com>
2017-11-19 01:26:45 -0500

    adding firebase

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
-const firebase = require('firebase');
-
 Roomchata.prototype.initFirebase = function() {
     this.auth = firebase.auth();
     this.database = firebase.database();
     this.storage = firebase.storage();
     this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
+
+window.onload = function() {
+    window.roomchata = new Roomchata();
+}
