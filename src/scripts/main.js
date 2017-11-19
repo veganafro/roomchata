a03e25e8fe8ee7795e066a42b186d13c19586f91
jeremymuhia <jmuhia80@gmail.com>
2017-11-19 16:21:44 -0500

    finally up and running with browserify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,1 @@
 const firebase = require('firebase');
-
-/*
-<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-database.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-firestore.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase-messaging.js"></script>
-*/
