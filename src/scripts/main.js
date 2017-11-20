f14315e4243c74428d85b0fda03d084205322ee9
jeremymuhia <jmuhia80@gmail.com>
2017-11-20 10:43:12 -0500

    finally writing client side things

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,22 @@
 const firebase = require('firebase');
 
+class Roomchata {
+    constructor() {
+        firebase.initializeApp({
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+        });
+        this.auth = firebase.auth();
+        this.storage = firebase.storage();
+        this.database = firebase.database();
+        this.auth.onAuthStateChanged(this.onAuthStateChanged().bind(this));
+    }
+}
+
 window.onload = function() {
-    firebase.initializeApp({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-    });
-    console.log(firebase.auth());
+    window.roomchata = new Roomchata();
 }
