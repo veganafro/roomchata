c9893d4adeb81aeb602856298f711835570b8823
jeremymuhia <jmuhia80@gmail.com>
2017-11-19 23:27:37 -0500

    trying so hard with this client side nonsense

--- src/app.js
+++ src/app.js
@@ -49,12 +49,4 @@ app.post('/search', function(request, response) {
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port', this.address().port, 'in context', app.settings.env);
-    console.log({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-    });
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,13 @@
 const firebase = require('firebase');
-console.log(process.env);
+console.log(process);
+
+window.onload = function() {
+    firebase.initializeApp({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    });
+}
