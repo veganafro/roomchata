b712028d0a7a9c6bb02703856f43b1c68663679b
jeremymuhia <jmuhia80@gmail.com>
2017-11-21 13:52:05 -0500

    stylin

--- src/views/base.jsx
+++ src/views/base.jsx
@@ -10,8 +10,8 @@ class Base extends React.Component {
                     {this.props.title}
                 </title>
 
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
-                <link rel="stylesheet" href="stylesheets/style.css">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en"></link>
+                <link rel="stylesheet" href="stylesheets/style.css"></link>
             </head>
             <body>
                 <h3 className='base_header'>roomchata</h3>
