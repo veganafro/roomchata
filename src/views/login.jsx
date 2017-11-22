00974c0d33cdd864e85052ff4998ec09287e8773
jeremymuhia <jmuhia80@gmail.com>
2017-11-21 21:06:01 -0500

    making login work

---
+++ src/views/login.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+const Base = require('./base');
+
+class Login extends React.Component {
+    render() {
+        return (
+            <Base title='Login'>
+            <h3 className='login_header'>roomchata</h3>
+            <form method='POST'>
+            </form>
+            </Base>
+        );
+    }
+}
