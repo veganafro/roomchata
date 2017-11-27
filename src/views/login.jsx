5bfc02471bb8b39d788b8be216e0452f391ec11f
jeremymuhia <jmuhia80@gmail.com>
2017-11-27 14:26:54 -0500

    built most of log in

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -7,7 +7,8 @@ class Login extends React.Component {
         return (
             <Base title='Login'>
             <h1 className='login_header'>roomchata</h1>
-            <form method='POST'>
+            <form method='POST'
+                name='login_form'>
                 <input
                     className='login_username_input'
                     type='text'
@@ -15,6 +16,21 @@ class Login extends React.Component {
                     placeholder='username...'
                     value={this.props.login_username_text}>
                 </input>
+                <br></br>
+                <input
+                    className='login_password_input'
+                    type='password'
+                    name='login_password_text'
+                    placeholder='password...'
+                    value={this.props.login_password_text}>
+                </input>
+                <br></br>
+                <button
+                    className='login_submit_input'
+                    type='submit'
+                    name='login_submit_button'>
+                    log in
+                </button>
             </form>
             </Base>
         );
